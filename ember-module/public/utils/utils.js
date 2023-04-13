c04f9747f91f1dc722dbe461086cc68bddcdae6e
Santo Shakil <shakil.mezbah@gmail.com>
2023-04-13 11:11:33 +0600

    ..

--- ember-module/public/utils/utils.js
+++ ember-module/public/utils/utils.js
@@ -1,18 +1,5 @@
-// import { inject as service } from '@ember/service';
-
-// export default class MyUtils {
-//   // @service store;
-
-//   async getMyModelData() {
-//     // let myModel = await this.store.findAll('text-input');
-//     // return myModel;
-//     console.log('getMyModelData');
-//   }
-// }
-
 console.log('utils.js');
 
-// fetch from http://0.0.0.0:4200/api/text-input/
 async function getMyModelData() {
   let response = await fetch('/api/text-input/');
   let data = await response.json();
